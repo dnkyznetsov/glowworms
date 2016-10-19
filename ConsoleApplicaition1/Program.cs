552eeb02d0fb6ea140f605f5f07eba211c73d155
dnkyznetsov <dnkyznetsov@gmail.com>
2016-10-19 18:24:13 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplicaition1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicaition1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"D:\gaz.txt");
+            var closeList = new List<float>();
+
+            foreach (var line in lines)
+            {
+                var closestr = line.Split(',')[7];
+                var closeInt = float.Parse(closestr.Replace('.', ','));
+                closeList.Add(closeInt);
+            }
+            var postiions = new List<int>();
+            var prev = 0;
+            foreach (var price in closeList)
+            {
+                var c = (int) price;
+                if (c != prev)
+                {
+                    prev = c;
+
+                }
+
+            }
+        }
+    }
+
+
+}
